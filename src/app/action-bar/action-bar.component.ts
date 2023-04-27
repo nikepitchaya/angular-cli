1d7a1b0d1d88466f8557772ad8b336af0503561e
nikepitchaya <103920925+nikepitchaya@users.noreply.github.com>
2023-04-27 23:10:23 +0700

    counter-event-blinding

--- src/app/action-bar/action-bar.component.ts
+++ src/app/action-bar/action-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-action-bar',
@@ -6,5 +6,31 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./action-bar.component.scss'],
 })
 export class ActionBarComponent {
- @Input() name: string = ""
+  count: number = 0;
+  @Input() name: string = '';
+  @Output() showCount = new EventEmitter();
+
+  plusNumber(): void {
+    this.count++;
+    this.emitCounter()
+  }
+  minusNumber(): void {
+    this.count--;
+    this.emitCounter()
+  }
+  emitCounter(): void {
+    if (this.name == 'Minus') {
+      let payload = {
+        count: this.count,
+        action: 'Minus',
+      };
+      this.showCount.emit(payload);
+    } else if (this.name == 'Plus') {
+      let payload = {
+        count: this.count,
+        action: 'Plus',
+      };
+      this.showCount.emit(payload);
+    }
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Counter } from 'src/interfaces/counter';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   money: number = 0;
   priceShabu: number = 0;
-  paymentPrice :number = 0;
+  paymentPrice: number = 0;
+  countPlus: number = 0;
+  countMinus: number = 0;
+
   calculateMoney(): void {
     this.paymentPrice = (this.priceShabu * 3) / 4;
+  }
+  showCount(data: Counter): void {
+    if (data.action == 'Plus') this.countPlus = data.count;
+    else if (data.action == 'Minus')  this.countMinus = data.count;
+    else console.log('Error');
   }
 }
